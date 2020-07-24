52e8a9a1eb918dd4f16f0cdfc9d7ab18c440c40e
Adam Weiner <adamcweiner@gmail.com>
2020-07-24 15:10:10 -0400

    not sure if mode is right but num_tel column is added correctly

--- scripts/find_num_tel.py
+++ scripts/find_num_tel.py
@@ -1,17 +1,52 @@
 import pandas as pd
 import os
 
-def find_num_tel(df, cn):
+def get_number_of_telomeres(df, cn):
 	""" Estimate the number of telomeres for each cell in df using the hmmcopy information in cn. """
 	# add column for number of telomeres
 	df['num_tel'] = None
-
+	print df.head()
+	print cn.head()
 	for key, row in df.iterrows():
+		print 'key', key
 		cell_id = os.path.basename(row['BAM_path']).split('.')[0]
-		cell_df = df.loc[cell_id]
+		cell_df = cn.loc[cell_id]
 		num_tel = 0
 		for chrom, group in cell_df.groupby('chr'):
-			num_tel += 2 * mode(group['state'])  # add chromosome arm's ploidy to running total
+			print 'chrom', chrom
+			temp = 2 * group['state'].mode().values[0]
+			print 'chrom', chrom, 'num', temp
+			num_tel += temp  # add chromosome arm's ploidy to running total
+		print 'num_tel', num_tel
 		df.loc[key, 'num_tel'] = num_tel
+
+	return df
+
+
+def find_num_tel(df, hmm_file_paths):
+	""" Load in hmmcopy results and estimate the number of telomeres. """
+	# make sure we have proper hmmcopy files if number of telomeres isn't already provided
+	try:
+		hmm_files = []
+		with open(hmm_file_paths, 'rb') as f:
+			for line in f:
+				hmm_files.append(line.strip())
+	except:
+		raise ValueError("Error when loading in hmmcopy files")
+
+	# read hmmfiles into 
+	cn_pieces = []
+	for f in hmm_files:
+		piece = pd.read_csv(
+			f, index_col=['chr', 'start', 'end', 'width', 'cell_id']
+		)
+		piece = piece[['reads', 'gc', 'map', 'copy', 'state']]
+		cn_pieces.append(piece)
+	cn = pd.concat(cn_pieces)
+	cn = cn.reset_index()
+	cn.set_index('cell_id', inplace=True)
+
+	# use hmmcopy info to estimate number of telomeres for each cell in df
+	df = get_number_of_telomeres(df, cn)
 
 	return df
--- telos.py
+++ telos.py
@@ -8,7 +8,7 @@ from scripts.estimate_lengths import estimate_lengths
 parser = argparse.ArgumentParser()
 parser.add_argument('-i', '--input_cells', type=str,
 	help='CSV file that stores `BAM_path`, `cov` (coverage, optional), and num_tel (number of telomeres, optional)')
-parser.add_argument('-h', '--hmmcopy', type=str, nargs='+',
+parser.add_argument('-hmm', '--hmmcopy', type=str, nargs='?',
 	help='paths to hmmcopy output for the libraries containing the input_cells')
 parser.add_argument('-o', '--output', type=str,
 	help='path for writing the output csv file')
@@ -32,22 +32,7 @@ def main():
 		print 'done estimating coverage'
 
 	if 'num_tel' not in df.columns:
-		# make sure we have proper hmmcopy files if number of telomeres isn't already provided
-		try:
-			cn_pieces = []
-			for f in args.hmmcopy:
-				piece = pd.read_csv(
-					f, index_col=['chr', 'start', 'end', 'width', 'cell_id']
-				)
-				piece = piece[['reads', 'gc', 'map', 'copy', 'state']]
-				cn_pieces.append(piece)
-			cn = pd.concat(cn_pieces)
-			cn = cn.reset_index()
-		except:
-			raise ValueError("Error when loading in hmmcopy files")
-
-		# use hmmcopy info to estimate number of telomeres for each cell in df
-		df = find_num_tel(df, cn)
+		df = find_num_tel(df, args.hmmcopy)
 		print df
 
 	estimate_lengths(df, args.output)
